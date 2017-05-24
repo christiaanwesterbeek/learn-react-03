af0f3c532deabed0bba5e2625387bcda4daf658a
Christiaan Westerbeek <chris@devotis.nl>
2017-05-24 12:57:28 +0200

    Done with 13th video - intercept route change

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
-import React from 'react';
+import React, { Component} from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link,
-  Switch,
-  Redirect
+  Prompt
 } from 'react-router-dom'
 
-const loggedin = false
-
-const Home = () => {
-  return <h1>Home</h1>
-}
-
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
-    <Link to="/menu/food">Food</Link>
-    <Link to="/menu/drinks">Drinks</Link>
-    <Link to="/menu/sides">Sides</Link>
-    <Route
-      path="/menu/:section"
-      render={({match}) => (
-        <h2>{match.params.section}</h2>
-      )}
-    />
-  </div>
+const Home = () => (
+  <h1>Home</h1>
 )
+
+class Form extends Component {
+  state = {dirty: false}
+  setDirty = () => this.setState({dirty: true})
+  render() {
+    return (
+      <div>
+        <h1>Form: {this.state.dirty ? 'dirty' : 'not dirty'}</h1>
+        <input type="text" onInput={this.setDirty} />
+        <Prompt
+          message="Data will be lost"
+          when={this.state.dirty}
+        />
+
+      </div>
+    )
+  }
+}
 
 const App = () => (
   <Router>
     <div>
       <Link to="/">Home</Link>
-      <Link to="/old/123">Old</Link>
-      <Link to="/new/456">New</Link>
-      <Link to="/menu ">Menu</Link>
-      <Link to="/protected">protected</Link>
+      <Link to="/form">Form</Link>
       <Route exact path="/" component={Home} />
-      <Route path="/menu" component={Menu} />
-      <Route path="/new/:str" render={({match}) => (
-        <h1>New: {match.params.str}</h1>
-      )} />
-      <Route path="/old/:str" render={({match}) => (
-        <Redirect to={`/new/${match.params.str}`} />
-      )} />
-      <Route path="/protected" render={() => (
-        loggedin
-        ? <h1>Welcome to the protected page</h1>
-        : <Redirect to={`/new/login`} />
-      )} />
+      <Route path="/form" component={Form} />
     </div>
   </Router>
 )
