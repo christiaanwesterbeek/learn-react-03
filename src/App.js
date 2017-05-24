a8c76a01dcb822eca23261c4d9f732708342ab6c
Christiaan Westerbeek <chris@devotis.nl>
2017-05-24 11:28:48 +0200

    Done with video 1-3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const isActiveFunc = (match, location) => {
+  console.log('isActiveFunc', match, location)
+  return match
 }
 
-export default App;
+const Links = () => {
+  return (
+    <nav>
+      <NavLink exact to="/">Home</NavLink>
+      <NavLink activeStyle={{color: 'green'}} to={{pathname: '/about'}}>About</NavLink>
+      <NavLink
+        isActive={isActiveFunc}
+        to="/contact">Contact</NavLink>
+    </nav>
+  )
+}
+
+const Home = (props) => {
+  console.log('props', props)
+  return <h1>Home</h1>
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <Links></Links>
+      <Route exact path="/" component={Home}></Route>
+      <Route
+        path="/about"
+        children={({match}) => (
+          match && <h1>About</h1>
+        )}></Route>
+      <Route path="/contact" render={() => <h1>Contact</h1>}></Route>
+    </div>
+  </Router>
+)
+
+export default App
