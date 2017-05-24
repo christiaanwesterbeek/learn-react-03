60dd82a47633791105d6b61c098a9ca1f1397889
Christiaan Westerbeek <chris@devotis.nl>
2017-05-24 12:28:29 +0200

    Done with 12th video - redirect

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Switch,
+  Redirect
 } from 'react-router-dom'
+
+const loggedin = false
 
 const Home = () => {
   return <h1>Home</h1>
@@ -28,9 +32,23 @@ const App = () => (
   <Router>
     <div>
       <Link to="/">Home</Link>
-      <Link to="/menu">Menu</Link>
+      <Link to="/old/123">Old</Link>
+      <Link to="/new/456">New</Link>
+      <Link to="/menu ">Menu</Link>
+      <Link to="/protected">protected</Link>
       <Route exact path="/" component={Home} />
       <Route path="/menu" component={Menu} />
+      <Route path="/new/:str" render={({match}) => (
+        <h1>New: {match.params.str}</h1>
+      )} />
+      <Route path="/old/:str" render={({match}) => (
+        <Redirect to={`/new/${match.params.str}`} />
+      )} />
+      <Route path="/protected" render={() => (
+        loggedin
+        ? <h1>Welcome to the protected page</h1>
+        : <Redirect to={`/new/login`} />
+      )} />
     </div>
   </Router>
 )
