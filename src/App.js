698e0fee107474e8f7a323f6319612c5c3516530
Christiaan Westerbeek <chris@devotis.nl>
2017-05-24 11:50:10 +0200

    Done with 8th video

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
   NavLink
 } from 'react-router-dom'
 
@@ -32,25 +33,27 @@ const App = () => (
   <Router>
     <div>
       <Links></Links>
-      <Route exact path="/" component={Home}></Route>
-      <Route
-        path="/about"
-        children={({match}) => (
-          match && <h1>About</h1>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route
+          path="/about"
+          children={({match}) => (
+            match && <h1>About</h1>
+          )}></Route>
+        <Route path="/:page?/:subpage?" render={({match}) => (
+          <div>
+            <h1>PAGE: {match.params.page || 'Home'}</h1>
+            <h2>SUBPAGE: {match.params.subpage || 'no subpage'}</h2>
+          </div>
         )}></Route>
-      <Route path="/contact" render={() => <h1>Contact</h1>}></Route>
-      <Route path="/:page?/:subpage?" render={({match}) => (
-        <div>
-          <h1>PAGE: {match.params.page || 'Home'}</h1>
-          <h2>SUBPAGE: {match.params.subpage || 'no subpage'}</h2>
-        </div>
-      )}></Route>
-      <Route path="/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)?/:subpage?" render={({match}) => (
-        <div>
-          <h1>paramA: {match.params.a }</h1>
-          <h2>paramB: {match.params.b}</h2>
-        </div>
-      )}></Route>
+        <Route path="/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)?/:subpage?" render={({match}) => (
+          <div>
+            <h1>paramA: {match.params.a }</h1>
+            <h2>paramB: {match.params.b}</h2>
+          </div>
+        )}></Route>
+        <Route render={() => <h1>Not found</h1>} />
+      </Switch>
     </div>
   </Router>
 )
